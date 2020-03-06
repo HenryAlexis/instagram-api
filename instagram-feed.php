aa2dba4aef1689cdba4ad9c075511fa54c6fd602
henrydoes <61861001+henrydoes@users.noreply.github.com>
2020-03-06 15:37:42 +1000

    Create instagram-feed.php

---
+++ instagram-feed.php
@@ -0,0 +1,34 @@
+$ch = curl_init();
+$url = "https://graph.instagram.com/me/media?fields=id%2Cpermalink%2Ccaption%2Cmedia_url%2Cmedia_type&access_token=".$InstaAccessToken;
+
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$response = curl_exec($ch);
+$err = curl_error($ch);
+
+curl_close($ch);
+
+if ($err) {
+  return "cURL Error #:" . $err;
+} else {
+  $response = json_decode($response, true);
+
+  if (isset($response["data"])) {
+        
+      // Insert new feed into table
+      $insert = SQLInsert::create('"InstagramFeed"');
+
+      foreach ($response["data"] as $data) {
+              $ID = $data["id"]; // ID instagram feed
+              $URL = $data["permalink"]; // URL of instagram feed, good for links
+              $Caption = $data["caption"]; // Caption, use for description of the instagram feed
+              $Image = $data["media_url"]; // The URL of the image, this can be inserted into a src attribute of a image tag in HTML
+              // DO SOMETHING WITH THESE VARIABLES
+      }
+
+      
+
+ }
